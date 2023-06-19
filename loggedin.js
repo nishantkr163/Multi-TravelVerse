5faaff77c3b7216634b4a182a686448b63ffce4d
maniRhombuz <manish@rhombuz.io>
2023-06-19 10:50:31 +0530

    final day submission

---
+++ loggedin.js
@@ -0,0 +1,8 @@
+let details = JSON.parse(localStorage.getItem("logged"));
+console.log(details)
+
+let detail = document.querySelector(".register");
+detail.addEventListener("click", function() {
+    let msg = document.createElement('div') ;
+    msg.setAttribute("class", "msg");
+})
